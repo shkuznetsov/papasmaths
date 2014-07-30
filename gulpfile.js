f0df08438f44494e37a30a2cdbff9011799298a3
Alexander Kuznetsov <sasha@monterosa.co.uk>
2014-07-30 14:56:36 +0100

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,128 @@
+var
+	fs = require('fs'),
+	path = require('path'),
+	gulp = require('gulp'),
+	concat = require('gulp-concat'),
+	rename = require('gulp-rename'),
+	uglify = require('gulp-uglify'),
+	gzip = require('gulp-gzip'),
+	sass = require('gulp-sass'),
+	mincss = require('gulp-minify-css'),
+	replace = require('gulp-replace'),
+	config = JSON.parse(fs.readFileSync('gulpconfig.json')),
+	pkg = JSON.parse(fs.readFileSync('package.json')),
+	args = require('yargs').argv,
+	options = {
+		paths: {
+			html: {
+				base: 'www/',
+				input: 'www/app.html',
+				output: 'index.html'
+			},
+			css: {
+				base: 'www/css/',
+				input: 'www/css/main.scss',
+				output: {
+					concat: 'build.css',
+					min: 'build.min.css',
+					gz: 'build.min.css.gz'
+				}
+			},
+			js: {
+				base: 'www/js/',
+				input: [
+					'www/js/libs/zepto.js',
+					'www/js/libs/purl.js',
+					'www/js/task.js'
+				],
+				output: {
+					concat: 'build.js',
+					min: 'build.min.js',
+					gz: 'build.min.js.gz'
+				}
+			},
+		},
+		uglify: {
+			preserveComments: 'some',
+			output: {
+				beautify: false,
+				ascii_only: true,
+				quote_keys: true
+			}
+		},
+		git: {
+			remote:
+		}
+	};
+
+gulp.task('build-js', function()
+{
+    return gulp.src(options.paths.js.input)
+		.pipe(concat(options.paths.js.output.concat))
+		.pipe(gulp.dest(options.paths.js.base))
+		.pipe(uglify(options.uglify))
+		.pipe(rename(options.paths.js.output.min))
+		.pipe(gulp.dest(options.paths.js.base))
+		.pipe(gzip())
+		.pipe(rename(options.paths.js.output.gz))
+		.pipe(gulp.dest(options.paths.js.base));
+});
+
+gulp.task('build-css', function()
+{
+    return gulp.src(options.paths.css.input)
+		.pipe(sass())
+		.pipe(rename(options.paths.css.output.concat))
+		.pipe(gulp.dest(options.paths.css.base))
+		.pipe(mincss())
+		.pipe(rename(options.paths.css.output.min))
+		.pipe(gulp.dest(options.paths.css.base))
+		.pipe(gzip())
+		.pipe(rename(options.paths.css.output.gz))
+		.pipe(gulp.dest(options.paths.css.base));
+});
+
+gulp.task('build-html', function()
+{
+	return gulp.src(options.paths.html.input)
+		.pipe(replace(options.paths.js.output.concat, options.paths.js.output.min + '?version=' + pkg.version))
+		.pipe(replace(options.paths.css.output.concat, options.paths.css.output.min + '?version=' + pkg.version))
+		.pipe(rename(options.paths.html.output))
+		.pipe(gulp.dest(options.paths.html.base));
+});
+
+gulp.task('build', ['build-js', 'build-css', 'build-html']);
+
+gulp.task('watch', function()
+{
+	var js = path.join(options.paths.js.base, '**', '*'),
+	    css = path.join(options.paths.css.base, '**', '*');
+
+	gulp.watch([js, css], ['build']);
+});
+
+gulp.task('bump', function (cb)
+{
+	var version = args['version'],
+		message = 'Release ' + version;
+
+	if (typeof version === 'undefined')
+	{
+		cb();
+	}
+	else
+	{
+		gulp.src('package.json')
+			.pipe(bump({version: version}))
+			.pipe(gulp.dest('./'))
+			.pipe(packageFilter.restore())
+			//.pipe(git.commit(message))
+			.on('end', function () {
+				git.commit(message, function () {
+					git.tag('v' + version, message, {}, function () {
+						git.push(config.git.remote, config.git.branch, {args: '--tags'}, cb);
+					});
+				});
+			});
+	}
+});
---
+++ www/js/task.js
@@ -0,0 +1,103 @@
+var Task = function ( spec )
+{
+	this.spec = spec;
+};
+
+Task.prototype.parse = function ( )
+{
+	var i, opPos, operators = [' ', '+', '-', '*', '/'];
+
+	this.spec = this.spec.trim();
+
+	for (i in operators) if ((opPos = this.spec.indexOf(operators[i])) > 0)
+	{
+		this.operator = i ? operators[i] : '+';
+		break;
+	}
+
+	if (!this.operator) throw 'Operator not found';
+
+	this.operands = [this.parseOperand(this.spec.substr(0, opPos)), this.parseOperand(this.spec.substr(opPos + 1))];
+
+	this.parsed = true;
+};
+
+Task.prototype.parseOperand = function ( operand )
+{
+	var objectNums = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+
+	operand = operand.trim();
+
+	if (operand == parseInt(operand))
+	{
+		return ['num', parseInt(operand)];
+	}
+
+	for (var i in objectNums) if (operand == objectNums[i])
+	{
+		return ['obj', ++i];
+	}
+
+	throw 'Operand "' + operand + '" wasn\'t recognised';
+};
+
+Task.prototype.build = function ( )
+{
+	if (!this.parsed)
+	{
+		try
+		{
+			this.parse();
+		}
+		catch (e)
+		{
+			return $('<div>').attr('class', 'error')
+				.append($('<div>').text("Erm, this is embarassing, but papa didn't understand this: " + this.spec))
+				.append($('<div>').text(e));
+		}
+	}
+
+	return $('<div>').attr('class', 'task')
+		.append(this.buildOperand(this.operands[0]))
+		.append(this.buildOperator(this.operator))
+		.append(this.buildOperand(this.operands[1]))
+		.append(this.buildOperator('='))
+		.append(this.buildAnswerCell());
+};
+
+Task.prototype.buildOperand = function ( operand )
+{
+	var $operand = $('<div>');
+
+	if (operand[0] == 'num')
+	{
+		$operand.text(operand[1]);
+	}
+	else
+	{
+		if (!this.object)
+		{
+			this.object = Math.ceil(Math.random() * 50) || 1;
+
+			if (this.object < 10) this.object = '0' + this.object;
+		}
+
+		$operand.attr('class', 'objects count-' + operand[1]);
+
+		for (var i = 1; i <= operand[1]; ++i) $operand.append($('<div>').css('backgroundImage', 'url(objects/' + this.object + '.png)'));
+	}
+
+	return $operand;
+};
+
+Task.prototype.buildOperator = function ( operator )
+{
+	var char = operator == '-' ? '&minus;' : (operator == '*' ? '&times;' : operator);
+
+	return $('<div>').attr('class', 'operator').html(char);
+};
+
+Task.prototype.buildAnswerCell = function ( )
+{
+	return $('<div>').attr('class', 'answer');
+};
