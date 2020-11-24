d746980fc363c0b1f29ffebacd6e87efcfda8569
Sasha <alexander@kuznetsov.by>
2020-11-24 10:28:12 +0000

    Replacing the code with the pm2 build

---
+++ precache-manifest.b9d4095c5e642fc1decf88784cdd9899.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c853806c5adc76e7c94c32745ce1232e",
+    "url": "/papasmaths/index.html"
+  },
+  {
+    "revision": "626a28c5a43fa7fe262a",
+    "url": "/papasmaths/static/css/main.418f94df.chunk.css"
+  },
+  {
+    "revision": "3603235c02884b6b560c",
+    "url": "/papasmaths/static/js/2.72a0e9eb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/papasmaths/static/js/2.72a0e9eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "626a28c5a43fa7fe262a",
+    "url": "/papasmaths/static/js/main.3a2cd30e.chunk.js"
+  },
+  {
+    "revision": "31d6991d65e2c514721e",
+    "url": "/papasmaths/static/js/runtime-main.39722208.js"
+  }
+]);
